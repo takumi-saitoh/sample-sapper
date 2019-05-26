b0376d40716ac11083031f69b3784db16020a35d
totoro081295 <totoro081295@gmail.com>
2019-05-26 22:03:52 +0900

    first commit

---
+++ src/routes/todo.svelte
@@ -0,0 +1,39 @@
+<style>
+.container {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+    width: 100%;
+}
+.header-title {
+    font-weight: lighter;
+    font-size: 3.5rem;
+}
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+}
+.main-container{
+    padding-bottom: 50px;
+}
+</style>
+
+<svelte:head>
+	<title>TODO APP</title>
+</svelte:head>
+<div class="container">
+    <header>
+        <h1 class="header-title">TO DO APP</h1>
+    </header>
+    <div class="row main-container">
+        <div class="col-md-8 col-xl-9">
+        aaa
+        </div>
+        <div class="col-md-4 col-xl-3">
+        bbb
+        </div>
+    </div>
+</div>
